dcf5fd34ada84b5c0acea69a593ab60db6deba95
say762 <217300999+say762@users.noreply.github.com>
2026-07-25 08:35:49 +0545

    improved the  passwords checking logic and moved it to a global function

--- main.cpp
+++ main.cpp
@@ -4,17 +4,31 @@
 #include<string>
 using namespace std;
 
+bool check_dictionary(const string& password){
+    ifstream common_file("common-passwords.txt",ios::in);
+
+    if(!common_file){
+        throw runtime_error("common-passwords.txt couldnt be opened!");
+        }
+
+        string common_pass;
+        while(common_file>>common_pass){
+            return true;
+        }
+        return false;
+
+}
+
 int main(){
    
-    string password,username;
-    string choice;
+    string password,username,choice;
+    try{
 do{
-    ifstream common_file("common-passwords.txt",ios::in);
+    
     ofstream user_file("passwords.txt",ios::app);
 
-    if(!common_file || !user_file){
-        cout<<"Error! Couldn't open the file"<<endl;
-        return 1;
+    if(!user_file){
+        throw runtime_error("Error! Couldn't write to 'password.txt' file");
     }
 
     cout<<"Enter your username"<<endl;
@@ -26,24 +40,13 @@ do{
     user_file<<setw(10)<<username<<" "<<setw(10)<<password<<endl;
     user_file.close();
 
-    string common_pass;
-    bool weak=false;
-    
-    while(common_file>>common_pass){
-        if(password==common_pass){
-            weak= true;
-            break;
-        }
+    if(check_dictionary(password)){
+        cout<<"\n WEAK PASSWORD (found in the common password list)"<<endl;
     }
-
-    if(weak){
-        cout<<"Weak password"<<endl;
-    }
-
     else{
-        cout<<"Strogn password"<<endl;
+        cout<<"\n Passwrod not foudn int he common password list"<<endl;
+        cout<<"\n Checking the stregnth of the password entered"<<endl;
     }
-    
     cout<<"Write yes to recheck your password or anything else to quit"<<endl;
     cin>>choice;
 
@@ -54,6 +57,7 @@ do{
 
   
     common_file.close();
+}
 }while(choice =="yes");
    return 0; 
 }
