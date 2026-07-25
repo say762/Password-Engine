bb4bd65c13de2aa24f824679256e9b7d0b50443f
say762 <217300999+say762@users.noreply.github.com>
2026-07-25 11:08:23 +0545

    working password module without the output formatting

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 #include<fstream>
 #include<iomanip>
 #include<string>
+#include<cctype>
 using namespace std;
+
+void Score(int score){
+    if(score==40){
+        cout<<"PASSWORD RATING: VERY STRONG "<<endl;
+    }
+
+    else if(score=20){
+        cout<<"PASSWORD RATING: WEAK"<<endl;
+    }
+
+    else{
+        cout<<"PASSWORD RATING: VERY WEAK (NEEDS TO BE CHANGED)"<<endl;
+    }
+}
 
 bool check_dictionary(const string& password){
     ifstream common_file("common-passwords.txt",ios::in);
@@ -13,18 +28,78 @@ bool check_dictionary(const string& password){
 
         string common_pass;
         while(common_file>>common_pass){
-            return true;
+            if(password==common_pass){
+                return true;
+            }
         }
         return false;
 
 }
 
-int main(){
-   
-    string password,username,choice;
-    try{
-do{
+void PasswordStrength(const string& password){
+
+    bool upper=false;
+    bool digit=false;
+    bool symbol=false;
+    int score=0;
     
+    if(password.length()<8){
+        cout<<"\n Length of the password is less than 8 characters"<<endl;
+    }
+    else{
+        cout<<"Password is 8 or 8+ characters"<<endl;
+        score += 10;
+    }
+
+    for(char c : password){
+        if(isupper(c)){
+            upper=true;
+        }
+
+         if(isdigit(c)){
+            digit=true;
+        }
+
+         if(ispunct(c)){
+            symbol=true;
+        }
+    }
+
+    if(!upper){
+        cout<<"Password does not have any uppercase characters."<<endl;
+    }
+     else{
+        cout<<"Password has uppercase characters"<<endl;
+        score+=10;
+    }
+   
+    if(!digit){
+        cout<<"Password does not have any digits"<<endl;
+    }
+     else{
+        cout<<"Password has digits"<<endl;
+        score+=10;
+    }
+
+    if(!symbol){
+        cout<<"Password does not have any symbols"<<endl;
+    }
+     else{
+        cout<<"Password has symbols"<<endl;
+        score+=10;
+    }
+     
+    
+    Score(score);
+
+    }
+
+int main(){
+     
+    try{
+        string user_input , pass_input , choice_input;
+do{
+
     ofstream user_file("passwords.txt",ios::app);
 
     if(!user_file){
@@ -32,32 +107,40 @@ do{
     }
 
     cout<<"Enter your username"<<endl;
-    cin>>username;
+    cin>>user_input;
 
     cout<<"Enter your password"<<endl;
-    cin>>password;
+    cin>>pass_input;
 
-    user_file<<setw(10)<<username<<" "<<setw(10)<<password<<endl;
+    user_file<<setw(10)<<user_input<<" "<<setw(10)<<pass_input<<endl;
     user_file.close();
 
-    if(check_dictionary(password)){
+    if(check_dictionary(pass_input)){
         cout<<"\n WEAK PASSWORD (found in the common password list)"<<endl;
     }
     else{
-        cout<<"\n Passwrod not foudn int he common password list"<<endl;
+        cout<<"\n Password not found in the common password list"<<endl;
         cout<<"\n Checking the stregnth of the password entered"<<endl;
+        PasswordStrength(pass_input);
     }
     cout<<"Write yes to recheck your password or anything else to quit"<<endl;
-    cin>>choice;
+    cin>>choice_input;
 
     
-    for (char &c:choice){
+    for(char &c:choice_input){
         c=tolower(c);
     }
 
   
-    common_file.close();
-}
-}while(choice =="yes");
+
+}while(choice_input =="yes");
+} 
+
+    catch(const exception &e){
+        cerr<<"Ensure that the 'common_passwords.txt' exists in the same directory"<<endl;
+        return 1;
+    }
+
+        cout<<"Exiting the password engine"<<endl;
    return 0; 
 }
